3d6613e2884b538033a17ee8b637a091a4eab381
Augusto F. Hack <hack.augusto@gmail.com>
2013-07-26 18:04:35 -0300

    list_decorator (shortcut decorator) and admin_url

--- admin_decorators/__init__.py
+++ admin_decorators/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from urllib import urlencode, splitquery
 from functools import wraps
 from django.utils.safestring import mark_safe
+from django.utils import six
 from django.template import Template, Context
+from django.core.urlresolvers import reverse
 
 try:
     from django.template.defaultfilters import truncatechars as _truncatechars
@@ -64,6 +67,18 @@ def limit_width(max_len):
         return inner
     return decorator
 
+def list_decorator(short_description=None, order_field=None, allow_tags=None, boolean=None):
+    def decorator(func):
+        if short_description is not None:
+            func.short_description = short_description
+        if order_field is not None:
+            func.admin_order_field = order_field
+        if allow_tags is not None:
+            func.allow_tags = allow_tags
+        if boolean is not None:
+            func.boolean = boolean
+        return func
+    return decorator
 
 def format_output(template_string):
     """
@@ -104,5 +119,44 @@ def apply_filter(filter_string):
     return decorator
 
 def external_url(func):
+    """
+    Example::
+
+        @external_url
+        def github_project(self, obj):
+            return 'http://github.com/{}'.format(obj.project)
+    """
     template = "<a href='{{ value }}' target='_blank'>{{ value }}</a>"
     return allow_tags(format_output(template)(func))
+
+def admin_url(viewname, displaytext=None):
+    """
+    Example::
+
+        @url('admin:app_model_chagelist')
+        def filter(self, obj):
+            return {'relation__exact__id': obj.relation.id}
+    """
+    keys = ('viewname', 'urlconf', 'args', 'kwargs', 'current_app')
+
+    def decorator(func):
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            res = func(*args, **kwargs)
+            args = dict((k, res[k]) for k in keys if k in res)
+            if viewname not in args:
+                args['viewname'] = viewname
+            url = reverse(**args)
+            querystring = urlencode(dict((k, v) for k, v in six.iteritems(res) if k not in args))
+            if querystring:
+                path, qs = splitquery(url)
+                if qs:
+                    querystring = qs + '&' + querystring
+                if path:
+                    url = '{}?{}'.format(path, querystring)
+                else:
+                    url = '/?' + querystring
+            return u"<a href='{}'>{}</a>".format(url, displaytext if displaytext is not None else url)
+        return allow_tags(inner)
+    return decorator
